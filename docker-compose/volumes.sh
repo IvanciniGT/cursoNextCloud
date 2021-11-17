e27e34400af47eb29f71a1375f7626de3f66d8ef
Ubuntu <ubuntu@ip-172-31-3-162.eu-west-1.compute.internal>
2021-11-17 08:01:33 +0000

    dia3

---
+++ docker-compose/volumes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo rm -rf ~/environment/datos
+mkdir -p /home/ubuntu/environment/datos/nextcloud-general
+mkdir /home/ubuntu/environment/datos/nextcloud-custom_apps
+mkdir /home/ubuntu/environment/datos/nextcloud-data
+mkdir /home/ubuntu/environment/datos/nextcloud-config
+mkdir /home/ubuntu/environment/datos/nextcloud-themes
+mkdir /home/ubuntu/environment/datos/mariadb
